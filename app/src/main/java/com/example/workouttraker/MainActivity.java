c550c98c620a3e734c1fd13a11d5995cd332f084
Roman Kryvenchenko <romankryk1803@gmail.com>
2019-03-30 13:15:02 +0200

    Initial commit

---
+++ app/src/main/java/com/example/workouttraker/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.workouttraker;
+
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button start, exit;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        start = findViewById(R.id.startButton);
+        exit = findViewById(R.id.startButtonExit);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, StartActivity.class));
+            }
+        });
+
+        // This is must be a intent(MainPageActivity)
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/workouttraker/StartActivity.java
@@ -0,0 +1,13 @@
+package com.example.workouttraker;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+
+public class StartActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
