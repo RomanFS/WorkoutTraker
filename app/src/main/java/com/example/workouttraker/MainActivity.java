17924c566fdd5f49c2b9319d847ba23f7b903545
Aleksey <lexa.grydnistiy123@gmail.com>
2019-03-31 16:35:45 +0300

    onClick refactoring

--- app/src/main/java/com/example/workouttraker/MainActivity.java
+++ app/src/main/java/com/example/workouttraker/MainActivity.java
@@ -15,20 +15,10 @@ public class MainActivity extends AppCompatActivity {
         Button start = findViewById(R.id.startButton);
         Button exit = findViewById(R.id.startButtonExit);
 
-        start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, StartActivity.class));
-            }
-        });
+        start.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, StartActivity.class)));
 
         // This is must be a intent(MainPageActivity)
-        exit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onBackPressed();
-            }
-        });
+        exit.setOnClickListener(v -> onBackPressed());
     }
 
 }
