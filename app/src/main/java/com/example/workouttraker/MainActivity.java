354a8e2fe5d8f788203b44e3577ac4abe7db1f58
Roman Kryvenchenko <romankryk1803@gmail.com>
2019-03-31 10:02:47 +0300

    don't work

--- app/src/main/java/com/example/workouttraker/MainActivity.java
+++ app/src/main/java/com/example/workouttraker/MainActivity.java
@@ -1,24 +1,19 @@
 package com.example.workouttraker;
 
 import android.content.Intent;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button start, exit;
-
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        start = findViewById(R.id.startButton);
-        exit = findViewById(R.id.startButtonExit);
+        Button start = findViewById(R.id.startButton);
+        Button exit = findViewById(R.id.startButtonExit);
 
         start.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/workouttraker/StartActivity.java
+++ app/src/main/java/com/example/workouttraker/StartActivity.java
@@ -2,12 +2,45 @@ package com.example.workouttraker;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class StartActivity extends FragmentActivity {
+public class StartActivity extends AppCompatActivity implements OnClickListener {
+    final Button back = findViewById(R.id.back);
+    final Button btContinue = findViewById(R.id.bt_continue);
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        // Button Call
+        back.setOnClickListener(this);
+        btContinue.setOnClickListener(this);
+
+        // Fragment Viewer
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        FragmentStart1 fragment = new FragmentStart1();
+        fragmentTransaction.add(R.id.container, fragment);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.back:
+                onBackPressed();
+                break;
+            case R.id.bt_continue:
+                Toast.makeText(StartActivity.this, "Next Fragment", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
